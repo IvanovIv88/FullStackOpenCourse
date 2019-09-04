94307219cca6e00880e0248fe47dd9de47f50cf7
Ivan Ivanov <ivani@starsgroup.com>
2019-09-04 10:28:52 +0300

    Add Part component and refacotr Content component

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Header from './components/Content';
+import Content from './components/Content';
+import Total from './components/Total';
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -15,16 +17,8 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
 }
