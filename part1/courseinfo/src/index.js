8fda3b7e5c16667e929e589babef38e2365c8139
Ivan Ivanov <ivani@starsgroup.com>
2019-09-04 15:37:24 +0300

    Change course and parts into a single obj step5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -6,8 +6,9 @@ import Content from './components/Content';
 import Total from './components/Total';
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
     {
       name: 'Fundamentals of React',
       exercises: 10
@@ -20,13 +21,13 @@ const App = () => {
       name: 'State of a component',
       exercises: 14
     }
-  ]
+  ]}
 
   return (
     <div>
-      <Header course={course}/>
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name}/>
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
