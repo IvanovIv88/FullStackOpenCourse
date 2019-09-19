149e61b4067261876b4ef2b71ede505368fc84cc
Ivan Ivanov <ivani@starsgroup.com>
2019-09-19 12:35:43 +0300

    Add the requirements for the phonebook step4 and 5

--- part2/thephonebook/src/App.js
+++ part2/thephonebook/src/App.js
@@ -2,16 +2,18 @@ import React, { useState } from 'react'
 
 import Persons from './components/Persons';
 import PersonForm from './components/PersonForm';
-// import PersonForm from './components/PersonForm';
-
-
+import Filter from './components/Filter';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456'}
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -19,6 +21,10 @@ const App = () => {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
   }
 
   const addName = (event) => {
@@ -43,6 +49,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Filter newFilter={newFilter} handleFilterChange={handleFilterChange} />
+      <h3>Add a new</h3>
       <PersonForm
         addName={addName}
         newName={newName}
@@ -51,7 +59,7 @@ const App = () => {
         handleNumberChange={handleNumberChange}
       />
       <h2>Numbers</h2>
-      <Persons persons={persons} />
+      <Persons persons={persons} newFilter={newFilter}/>
     </div>
   )
 }
--- part2/thephonebook/src/components/Persons.js
+++ part2/thephonebook/src/components/Persons.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
-const Persons = ({persons}) => {
-    return (
+const Persons = ({ persons, newFilter }) => {
+    if (newFilter) {
+        const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
+
+        return (
+            <>
+                {filteredPersons.map(person => <div key={person.name}>{`${person.name} ${person.number}`}</div>)}
+            </>
+        )
+    } else {
+        return(
         <>
             {persons.map(person => <div key={person.name}>{`${person.name} ${person.number}`}</div>)}
         </>
-    )
+        )
+    }
 }
 
 export default Persons;
